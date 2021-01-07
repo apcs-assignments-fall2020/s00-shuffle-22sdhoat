9c3598a850d6cc9b0f62c16161b0fd4b348156ef
22sdhoat <22sdhoat@athenian.org>
2021-01-07 03:13:14 +0000

    completed the 2 shuffle methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,16 +2,35 @@ public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int even_c = 0;
+        int odd_c = arr.length/2;
+        int[] real = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                real[i] += arr[even_c];
+                even_c++;
+            }
+            else {
+                real[i] += arr[odd_c];
+                odd_c++;
+            }
+        }
+        return real;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+        int placement = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int selection = (int)(Math.random() * arr.length);
+            placement = arr[i];
+            arr[i] = arr[selection];
+            arr[selection] = placement;
+        }
+        return arr;
     }
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
     }
 }
